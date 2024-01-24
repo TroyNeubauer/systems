9425b483bce103f292e1f80ebe503d55c2bb26f9
Troy Neubauer <troyneubauer@gmail.com>
2024-01-24 12:49:28 -0500

    try to add davinchi resovle

--- flake.nix
+++ flake.nix
@@ -8,26 +8,33 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      battlestation = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        config.allowUnfree = true;
+        inherit system;
+      };
+    in {
+      nixosConfigurations = {
+        battlestation = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
 
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+              # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
 
-            home-manager.users.troy = import ./home/home.nix {
-              pkgs = nixpkgs.legacyPackages."x86_64-linux";
-              # inherit (home) nixpkgs;
-            };
-          }
-        ];
+              home-manager.users.troy = import ./home/home.nix {
+                inherit pkgs;
+                # pkgs = nixpkgs.legacyPackages."x86_64-linux";
+                # inherit (home) nixpkgs;
+              };
+            }
+          ];
+        };
       };
     };
-  };
 }
