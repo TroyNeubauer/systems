133fd4d1ad2f8189e298041fa8adc7f036a5fde5
Troy Neubauer <troyneubauer@gmail.com>
2024-03-25 16:45:48 -0400

    update alacritty config

--- home/features/desktop/alacritty.nix
+++ home/features/desktop/alacritty.nix
@@ -1,20 +1,6 @@
 { lib, pkgs, config, ... }:
-let alacritty_themes = lib.fetchFromGithub {
-  owner = "alacritty";
-  repo = "alacritty-theme";
-  rev = "c2369cd1ec555c8dba7ea39bd059b7c036f1e637";
-};
-in {
+{
   programs.alacritty = {
     enable = true;
-
-    settings = ''
-      [font.normal]
-        family: Jetbrains Mono
-        size = 13;
-      live_config_reload: true
-
-    '';
-      # ${alacritty_themes}/themes/catppuccin_mocha.toml
   };
 }
